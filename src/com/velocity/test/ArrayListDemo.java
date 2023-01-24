471ddf1c1c40b14b3517799d4bb870d16b0d572c
anita <anubhui27@gmail.com>
2023-01-25 00:09:42 +0530

    implement ArrayList

--- src/com/velocity/test/ArrayListDemo.java
+++ src/com/velocity/test/ArrayListDemo.java
@@ -1,10 +1,15 @@
 package com.velocity.test;
 
+import java.util.ArrayList;
+
 public class ArrayListDemo {
 
 	public static void main(String[] args) {
-		System.out.println("hello");
-
+		ArrayList<Integer>al=new ArrayList<Integer>();
+		al.add(10);
+		al.add(20);
+		al.add(30);
+		System.out.println(al);
+		
 	}
-
 }
